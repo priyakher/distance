ec0e62ccf76796ba8befb4a64039af5ba032dc86
priyakher <pkher@mit.edu>
2018-04-04 10:31:34 -0400

    first comit

--- exercise.js
+++ exercise.js
@@ -5,7 +5,7 @@ exercise.distance = function(p1,p2){
     //-------------------
     //---- Your Code ----
     //-------------------    
-    return 'Error: 1st function not implemented';    
+    return (Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y))) ;    
 };
 
 // calculate distance between new point and existing
@@ -13,15 +13,25 @@ exercise.distances = function(newPoint,data){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 2nd function not implemented';
+
+   data.forEach(function(ele){  ele.distance= exercise.distance(newPoint,ele);});
+    return data;
 };
 
 // find k closest points
 exercise.findClosest = function(k,data){
     //-------------------
     //---- Your Code ----
-    //------------------- 
-    return 'Error: 3rd function not implemented';    
+    //-------------------
+
+   data.sort(function(a, b){return a.distance - b.distance});
+
+   var result=[];
+
+   for(var i=0;i<k;i++)
+    result[i]=data[i]
+
+    return result;    
 };
 
 module.exports = exercise;
